16a877d2da8f54da5c0fc4b0879e823cffb74644
Jose López <sejosin89@gmail.com>
2020-10-27 19:41:44 +0100

    version para subir a github

--- src/pages/ChronologyPage/ChronologyPage.jsx
+++ src/pages/ChronologyPage/ChronologyPage.jsx
@@ -6,6 +6,8 @@ import axios from "axios";
 import Footer from '../../shared/components/Footer/Footer'
 import Header from '../../shared/components/Header/Header'
 
+let allCharacters = [];
+
 export default function ChronologyPage() {
 
     const [characters, setCharacters] = useState([]);
@@ -14,35 +16,54 @@ export default function ChronologyPage() {
     const [maxAge, setMaxAge] = useState("");
 
     const [orderAsc, setOrderAsc] = useState(true);
-
+    
+    // Funciones que calculan la edad maxima y minima del array de personajes
+    const getOlder = (arr) => arr.reduce((prev, curr) => prev.age.age > curr.age.age ? prev : curr).age.age;
+    const getYounger = (arr) => arr.reduce((prev, curr) => prev.age.age < curr.age.age ? prev : curr).age.age;
 
     useEffect(() => {
         axios.get(process.env.REACT_APP_BACK_URL + "characters").then((res) => {
         //   con el item.age?.age elimina todos los item.age en caso de que sean false, y si no entra en item.age.age y mira si son false
-          const charactersAge = res.data.filter(item => !!item.age?.age);
-          setCharacters(charactersAge);
-          console.log(res.data[0])
-          setMinAge(charactersAge.reduce((prev, curr) => prev.age.age < curr.age.age ? prev : curr).age.age);
-          setMaxAge(charactersAge.reduce((prev, curr) => prev.age.age > curr.age.age ? prev : curr).age.age);
+          allCharacters = res.data.filter(item => item.age?.age);
+          setCharacters(allCharacters);
+          setMinAge(getYounger(allCharacters));
+          setMaxAge(getOlder(allCharacters));
         });
       }, []);
+      
+      
+    // Coge el input de la barra de busqueda y se lo asigna a la variable de estado "filter"
+    const handleOnSearch = (match) => {
+        const inputWord = match.currentTarget.value;
+        // Filtra el array en funcion de lo que vaya valiendo la variable de estado "filter" que se va actualizando en la funcion handleOnSearch
+        const filteredCharacters = allCharacters.filter(({ name }) => name.toLowerCase().includes(inputWord.toLowerCase()));
+        setCharacters(filteredCharacters);
+        // Checkeamos que filteredCharacters no este vacio
+        if (filteredCharacters.length) {
+            console.log(filteredCharacters)
+            setMinAge(getYounger(filteredCharacters));
+            setMaxAge(getOlder(filteredCharacters));
+        }
+    }
 
+    // Ordena el array en orden asc o desc dependiendo de la variable de estado orderAsc
+    const sortChar = (arr) => arr.sort((a, b) => orderAsc ? (a.age.age - b.age.age) :  (b.age.age - a.age.age));
 
     return (
         <>
-            <Header charac={characters}/>
+            <Header charac={characters} onSearch={handleOnSearch}/>
             <div class="container make-space scrollBar">
                 <div id="button-container" className="d-flex justify-content-center align-items-end">
                     {/* con el setOrderAsc(!orderAsc) cambiamos el estado de "orderAsc al contrario del actual" */}
                     <button onClick={() => setOrderAsc(!orderAsc)} id="button-age">{ orderAsc ? minAge : maxAge }</button>
                 </div>
                 <div className="d-flex justify-content-center">
-                    <div className="d-flex flex-column align-items-center">
+                    <div className="d-flex flex-column align-items-center column-contain">
                         <div id="vl" className="d-flex flex-column align-items-end justify-content-between">
                         {orderAsc ? <span class="arrow up"></span> : <span class="arrow down"></span>}
                         {/* {!orderAsc ? <span class="arrow down"></span> : <span></span>} */}
                         </div>
-                        {characters.filter((item, index) => index % 2 === 0).sort((a, b) => orderAsc ? (a.age.age - b.age.age) :  (b.age.age - a.age.age)).map((character, i) => (
+                        {sortChar(characters).filter((item, index) => index % 2 === 0).map((character, i) => (
                             <div id="cont-1" className="d-flex flex-column align-items-center w-100">
                                 <p className="mb-1">{character.age.age}</p>
                                 <p className="mb-1">{character.name}</p>
@@ -52,9 +73,9 @@ export default function ChronologyPage() {
                             </div>
                         ))}
                     </div>
-                    <div className="d-flex flex-column align-items-center">
+                    <div className="d-flex flex-column align-items-center column-contain">
                         <div id="space-div"></div>
-                        {characters.filter((item, index) => index % 2 !== 0).sort((a, b) => orderAsc ? (a.age.age - b.age.age) :  (b.age.age - a.age.age)).map((character, i) => (
+                        {sortChar(characters).filter((item, index) => index % 2 !== 0).map((character, i) => (
                             <div id="cont-2" className="d-flex flex-column align-items-center w-100">
                                 <p className="mb-1">{character.age.age}</p>
                                 <p className="mb-1">{character.name}</p>
